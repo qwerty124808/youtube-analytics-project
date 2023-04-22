b2b12e9e05959a3f46d01f97ff62d85942a85102
altarev_da <altarev_da@nlmk.com>
2023-04-22 18:36:24 +0300

    final_h_w_2

--- src/channel.py
+++ src/channel.py
@@ -11,8 +11,37 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.channel_id = channel_id
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]['description']
+        self.url = "https://www.youtube.com/" + self.channel["items"][0]["snippet"]["customUrl"]
+        self.subscribers_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
+        
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(self.channel)
-        
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+    def to_json(self, name):
+        write_in_json = {
+                         "id": self.channel_id,
+                         "title": self.title,
+                         "description": self.description,
+                         "url": self.url,
+                         "subscribersCount": self.subscribers_count,
+                         "videoCount": self.video_count,
+                         "viewCount": self.view_count
+                        }
+        with open(f"{name}", "w",     encoding="utf-8") as file:
+            json.dump(write_in_json, file, indent=4, ensure_ascii=False)
+
+if __name__ == "__main__":
+    test = Channel("UCMCgOm8GZkHp8zJ6l7_hIuA")
+    print(test.title)
+    test.to_json("test_2.json")
