1796c676e3bd8af80985fa360eb0f0cfde519c5b
altarev_da <altarev_da@nlmk.com>
2023-04-24 21:27:55 +0300

    final_hw_3

--- src/channel.py
+++ src/channel.py
@@ -15,9 +15,9 @@ class Channel:
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.description = self.channel["items"][0]["snippet"]['description']
         self.url = "https://www.youtube.com/" + self.channel["items"][0]["snippet"]["customUrl"]
-        self.subscribers_count = self.channel["items"][0]["statistics"]["subscriberCount"]
-        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
-        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
+        self.subscribers_count = int(self.channel["items"][0]["statistics"]["subscriberCount"])
+        self.video_count = int(self.channel["items"][0]["statistics"]["videoCount"])
+        self.view_count = int(self.channel["items"][0]["statistics"]["viewCount"])
         
 
     def print_info(self) -> None:
@@ -41,4 +41,32 @@ class Channel:
         with open(f"{name}", "w",     encoding="utf-8") as file:
             json.dump(write_in_json, file, indent=4, ensure_ascii=False)
 
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, objeckt_2):
+        return self.subscribers_count+ objeckt_2.subscribers_count
+    
+    def __sub__(self, objeckt_2):
+        return self.subscribers_count - objeckt_2.subscribers_count
+
+    def __lt__(self, objeckt_2):
+        return self.subscribers_count < objeckt_2.subscribers_count
+
+    def __le__(self, objeckt_2):
+        return self.subscribers_count <= objeckt_2.subscribers_count
+
+    def __gt__(self, objeckt_2):
+        return self.subscribers_count > objeckt_2.subscribers_count
+
+    def __ge__(self, objeckt_2):
+        return self.subscribers_count >= objeckt_2.subscribers_count
+
+    def __eq__(self, objeckt_2):
+        return self.subscribers_count == objeckt_2.subscribers_count
+
+test_1 = Channel("UCMCgOm8GZkHp8zJ6l7_hIuA")
+test_2 = Channel("UC1eFXmJNkjITxPFWTy6RsWg")
+print(test_1 + test_2)
+print(test_1 - test_2)
 
